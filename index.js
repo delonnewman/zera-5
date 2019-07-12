1c53f166bdcfbbb72018d8f9f9232869a7fc85ec
Delon Newman <delnewman@salud.unm.edu>
2019-07-11 23:08:26 -0600

    added zera.js renamed js.node to zera.js.node, added zera.js/json-read-str

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('./src/zera/runtime.js');
 zera.loadFile("./src/zera/core.zera");
+zera.loadFile("./src/zera/js.zera");
 zera.loadFile("./src/zera/js/node.zera");
 zera.loadFile("./src/zera/compiler/js.zera");
