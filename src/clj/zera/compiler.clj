c1866e5c74e8cb504839a595789ae9063434414f
Delon Newman <contact@delonnewman.name>
2020-04-10 20:34:40 -0600

    WIP: Added identifiers working on quote

--- src/clj/zera/compiler.clj
+++ src/clj/zera/compiler.clj
@@ -1,11 +1,34 @@
-(ns zera.compiler)
+(ns zera.compiler
+  (:require [clojure.spec.alpha :as s]))
 
-(deftype Env [vars parent])
+(defprotocol IEnv
+  (varMap [this])
+  (resetVars [this vars]))
+
+(deftype Env [^:volatile-mutable vars parent]
+  IEnv
+  (varMap [this] (.-vars this))
+  (resetVars [this vars]
+    (set! (.-vars this) vars)))
+
+(defn env?
+  [x] (instance? x Env))
+
+(s/def ::env env?)
 
 (defn env
   ([] (Env. {} nil))
   ([vars] (Env. vars nil))
   ([vars parent] (Env. vars parent)))
+
+(defn lookup
+  [scope sym]
+  (if ((.varMap scope) sym)
+    scope))
+
+(defn define [scope sym value]
+  (.resetVars scope (assoc (.varMap scope) sym value))
+  scope)
 
 (defn emit-comma-list
   [xs]
@@ -21,6 +44,7 @@
 
 ; TODO: add more advanced string features
 (def emit-self-evaluating pr-str)
+(def quote-self-evaluating pr-str)
 
 (defn emit-keyword [kw]
   (let [ns (namespace kw)
@@ -28,6 +52,8 @@
     (if ns
       (str "zera.core.keyword(\"" ns "\", \"" nm "\")")
       (str "zera.core.keyword(null, \"" nm "\")"))))
+
+(def emit)
 
 (defn emit-vector [v env]
   (let [elems (->> v (map #(emit % env)) emit-comma-list)]
@@ -41,6 +67,16 @@
 
 (def tagged-value-or-application? list?)
 
+(defn emit-identifier
+  [sym env]
+  (if-let [scope (lookup env sym)]
+    (let [ns (namespace sym)
+          nm (name sym)]
+      (if ns
+        (str ns "." nm)
+        nm))
+    (throw (Exception. (str "Unknown variable " sym)))))
+
 (defn emit-application
   [form env]
   (let [args (->> (rest form)
@@ -48,16 +84,51 @@
                   (map #(emit % env)))]
     (str (emit (first form) env) ".call(null, " args ")")))
 
-(defn emit-tagged-value-or-application [form env]
+(def quote-sym 'quote)
+(def def-sym 'def)
+(def fn-sym 'fn)
+
+(defn emit-quote
+  [form]
+  (cond (nil? form) "null"
+        (self-evaluating? form) (quote-self-evaluating form)
+        ))
+
+(defn emit-def
+  [form] "")
+
+(defn emit-fn
+  [form] "")
+
+(defn emit-tagged-value-or-application
+  [form env]
   (let [x (first form)]
     (if (symbol? x)
       (case x
-        'quote (emit-quote form)
-        'def   (emit-def form)
-        'fn    (emit-fn form env)
+        quote-sym (emit-quote form)
+        def-sym   (emit-def form)
+        fn-sym    (emit-fn form env)
+        (emit-application form env))
       (emit-application form env)
       ))
   )
+
+(def js? string?)
+
+(s/def ::form
+  (s/or :nil nil?
+        :string string?
+        :number number?
+        :keyword keyword?
+        :symbol symbol?
+        :vector vector?
+        :map map?
+        :list list?
+        :set set?))
+
+(s/fdef emit
+  :args (s/cat :form ::form :env env?)
+  :ret js?)
 
 (defn emit [form env]
   (cond (nil? form) "null"
@@ -65,12 +136,15 @@
         (keyword? form) (emit-keyword form)
         (vector? form) (emit-vector form env)
         (map? form) (emit-map form env)
+        (symbol? form) (emit-identifier form env)
         (tagged-value-or-application? form)
           (emit-tagged-value-or-application form env)
         :else
           (throw (Exception. (str "Unknown form " form)))))
 
 (def ^:private top (env))
+
+(define top 'a 1)
 
 (defn compile
   ([form] (emit form top))
