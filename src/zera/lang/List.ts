83dfc6b5ad41a107b9a22ca384f082d0eb92c1bc
Delon Newman <contact@delonnewman.name>
2021-06-15 14:08:20 -0600

    Remove List

--- src/zera/lang/List.ts
+++
@@ -1,9 +0,0 @@
-function List() { }
-List.$zera$tag = "zera.lang.List";
-List.$zera$isProtocol = true;
-List.$zera$protocols = { "zera.lang.Seq": Seq };
-List.prototype = Object.create(Seq.prototype);
-
-function isList(x) {
-    return isa(x, List);
-}
