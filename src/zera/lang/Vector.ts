48763e5ce38ee114b53df666d57ba9c2a7775861
Delon Newman <contact@delonnewman.name>
2021-06-15 14:02:46 -0600

    Port Vector

--- src/zera/lang/Vector.ts
+++ src/zera/lang/Vector.ts
@@ -1,95 +1,104 @@
-// Vectors
+import { IObj, AObj } from "./IObj";
+import { MetaData } from "./IMeta";
+import { ISeqable, ISeq } from "./Seq";
+import { Counted } from "./Counted";
+import { IFn } from "./AFn";
+import { str, prnStr, arrayToList, equals, isArray, isJSFn, first, next, seq } from "../core";
+
 // TODO: make Vector an abstract interface for PersistentVector and TransientVector
-function Vector(meta, rep) {
-    this.$zera$meta = meta;
-    this.rep = rep == null ? [] : rep;
-    ZeraType.call(this, Vector.$zera$tag, null, Vector.$zera$protocols);
+export class Vector extends AObj implements IObj, ISeqable, Counted, IFn {
+    private _rep: any[];
+
+    constructor(meta: MetaData | null, rep: any[]) {
+        super(meta);
+        this._rep = rep;
+    }
+
+    static EMPTY = new Vector(null, []);
+
+    toString() {
+        return str("[", this._rep.map(prnStr).join(" "), "]");
+    }
+
+    toArray() {
+        return this._rep;
+    }
+
+    // IMeta
+    meta() {
+        return this.$zera$meta;
+    }
+
+    // Seqable
+    seq(): ISeq {
+        return arrayToList(this._rep);
+    }
+
+    count() {
+        return this._rep.length;
+    }
+
+    // TODO: Define the interface that implements find
+    find(index: number): any {
+        return this._rep[index];
+    }
+
+    // alias find
+    nth = this.find;
+
+    contains(index: number): boolean {
+        return this.find(index) != null;
+    }
+
+    conj(x: any): Vector {
+        return new Vector(null, this._rep.concat(x));
+    }
+
+    // Array
+    indexOf(value: any): number {
+        return this._rep.indexOf(value);
+    }
+
+    findIndex(f: (element: any, index: number | undefined, array: any[] | undefined) => unknown): number {
+        return this._rep.findIndex(f);
+    }
+
+    // Fn
+    apply(_: any, args: any[]): any {
+        return this.invoke.apply(this, args);
+    }
+
+    call(_: any, ...args: any[]): any {
+        return this.invoke.apply(this, args);
+    }
+
+    invoke(...args: any[]): any {
+        if (args.length !== 1) {
+            throw new Error(str("Wrong number of arguments got: ", args.length, ", expected: 1"));
+        }
+
+        return this.find(args[0]);
+    }
+
+    // Equals
+    equals(other: any): boolean {
+        var a, b, i;
+        if (!isVector(other)) {
+            return false;
+        } else if (this._rep.length !== other.rep.length) {
+            return false;
+        } else {
+            a = this._rep;
+            b = other.rep;
+            for (i = 0; i < a.length; i++) {
+                if (!equals(a[i], b[i])) return false;
+            }
+            return true;
+        }
+    }
 }
 
-Vector.$zera$tag = Sym.intern("zera.lang.Vector");
-Vector.$zera$isType = true;
-Vector.$zera$protocols = { "zera.lang.IObj": IObj };
-Vector.prototype = Object.create(IObj.prototype);
-
-Vector.EMPTY = new Vector(null, []);
-
-Vector.prototype.toString = function() {
-    return str("[", this.rep.map(prnStr).join(" "), "]");
-};
-
-Vector.prototype.toArray = function() {
-    return this.rep;
-};
-
-// IMeta
-Vector.prototype.meta = function() {
-    return this.$zera$meta;
-};
-
-// Seqable
-Vector.prototype.seq = function() {
-    return arrayToList(this.rep);
-};
-
-Vector.prototype.count = function() {
-    return this.rep.length;
-};
-
-Vector.prototype.find = function(k) {
-    return this.rep[k];
-};
-
-Vector.prototype.contains = function(k) {
-    return this.rep[k] != null;
-};
-
-Vector.prototype.nth = Vector.prototype.find;
-
-Vector.prototype.conj = function(x) {
-    return new Vector(null, this.rep.concat(x));
-};
-
-// Array
-Vector.prototype.indexOf = function(v) {
-    return this.rep.indexOf(v);
-};
-
-Vector.prototype.findIndex = function(f) {
-    return this.rep.findIndex(f);
-};
-
-// Fn
-Vector.prototype.apply = function(obj, args) {
-    if (args.length !== 1) {
-        throw new Error(
-            str(
-                "Wrong number of arguments got: ",
-                args.length,
-                ", expected: 1"
-            )
-        );
-    }
-    return this.find(args[0]);
-};
-
-// Equals
-Vector.prototype.equals = function(other) {
-    var a, b, i;
-    if (!isVector(other)) {
-        return false;
-    } else if (this.rep.length !== other.rep.length) {
-        return false;
-    } else {
-        a = this.rep;
-        b = other.rep;
-        for (i = 0; i < a.length; i++) {
-            if (!equals(a[i], b[i])) return false;
-        }
-        return true;
-    }
-};
-
-function nth(v, n) {
+export function nth(v: any, n: number): any {
     if (isArray(v)) return v[n];
     else if (isJSFn(v.nth)) return v.nth(n);
     else {
@@ -99,15 +108,15 @@ function nth(v, n) {
     }
 }
 
-function isVector(x) {
+export function isVector(x: any): boolean {
     return x instanceof Vector;
 }
 
-function vector() {
-    return new Vector(null, Array.prototype.slice.call(arguments));
+export function vector(...args: any[]): Vector {
+    return new Vector(null, args);
 }
 
-function vec(s) {
+export function vec(s: ISeqable): Vector {
     var v = Vector.EMPTY,
         s_ = seq(s),
         x;
